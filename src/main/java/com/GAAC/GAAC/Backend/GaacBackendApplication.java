e57bd6574b6e6cb7028936709d66d1166fcd414e
panakantipranay <panakantipranay@gmail.com>
2025-12-11 22:13:15 +0530

    "Add basic configuration and required files"

---
+++ src/main/java/com/GAAC/GAAC/Backend/GaacBackendApplication.java
@@ -0,0 +1,13 @@
+package com.GAAC.GAAC.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaacBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaacBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GAAC/GAAC/Backend/GaacBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GAAC.GAAC.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaacBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
