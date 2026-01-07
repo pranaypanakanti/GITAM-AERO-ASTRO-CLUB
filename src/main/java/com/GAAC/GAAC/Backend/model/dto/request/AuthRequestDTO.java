d9cf72eb2028da683fabad78066ecde368350ed8
panakantipranay <panakantipranay@gmail.com>
2026-01-07 22:44:45 +0530

    "Add JWT tokens"

---
+++ src/main/java/com/GAAC/GAAC/Backend/model/dto/request/AuthRequestDTO.java
@@ -0,0 +1,15 @@
+package com.GAAC.GAAC.Backend.model.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequestDTO {
+    private String email;
+    private String password;
+}
